fc0e6bc569947b00304470ef7f8243df2fb166ab
Dinilla Cameron Paulse <ST10434929@vcconnect.edu.za>
2026-05-13 19:31:22 +0200

    Implemented memory system.

---
+++ ChatMemory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CybersecurityAwarenessBotGUI
+{
+    public class ChatMemory
+    {
+        private List<string> messages = new List<string>();
+
+        public void Store(string msg)
+        {
+            messages.Add(msg);
+        }
+
+        public string GetLast()
+        {
+            if (messages.Count == 0)
+                return "Nothing stored yet.";
+
+            return messages[messages.Count - 1];
+        }
+    }
+}
