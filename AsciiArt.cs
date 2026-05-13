1b18b4f3a4a3baa56a4472e66631b4811e24f643
Dinilla Cameron Paulse <ST10434929@vcconnect.edu.za>
2026-05-13 19:34:22 +0200

    Added ASCII branding display system

---
+++ AsciiArt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Figgle;
+
+namespace CybersecurityAwarenessBotGUI
+{
+    public class AsciiArt
+    {
+        public static void Show()
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render("Cybersecurity Bot"));
+        }
+    }
+}
