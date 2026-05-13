8ef539c71a8a4ecefa50a4567bec72083aca800d
Dinilla Cameron Paulse <ST10434929@vcconnect.edu.za>
2026-05-13 19:48:15 +0200

    Completed the first part.

---
+++ BotEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CybersecurityAwarenessBotGUI
+{
+    public class BotEngine
+    {
+        private ChatMemory memory = new ChatMemory();
+        private ResponseBank responses = new ResponseBank();
+
+        public string Process(string input)
+        {
+            memory.Store(input);
+
+            // SENTIMENT DETECTION
+            if (input.Contains("worried"))
+                return "It's okay to feel worried. Let me help you step by step.";
+
+            if (input.Contains("frustrated"))
+                return "I understand — cybersecurity can be confusing.";
+
+            if (input.Contains("curious"))
+                return "Great! Let’s explore cybersecurity together.";
